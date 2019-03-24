b6ac5fe13e18e4e93f84e099408d0934e89ec4d9
crossjames934 <crossjames934@gmail.com>
2019-03-24 19:09:39 +0000

    production build

---
+++ build/precache-manifest.6d8d09fff92af1b9e7e3a698db2d2dbf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b5ca8ac746021d6063065e9d924569f",
+    "url": "/static/media/firearm.4b5ca8ac.svg"
+  },
+  {
+    "revision": "377c64ad5175cc46e1d6",
+    "url": "/static/css/main.1f6b192f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "20ed2d7176e059a90f1c",
+    "url": "/static/js/2.6b41687d.chunk.js"
+  },
+  {
+    "revision": "a6978a280e628113e2eec527525990ed",
+    "url": "/static/media/footprints.a6978a28.svg"
+  },
+  {
+    "revision": "0409f2eb4a41e1f83ec031d60b19b2be",
+    "url": "/static/media/drugs.0409f2eb.svg"
+  },
+  {
+    "revision": "377c64ad5175cc46e1d6",
+    "url": "/static/js/main.7c0caa08.chunk.js"
+  },
+  {
+    "revision": "9d6efa39fc2e96c12647dd9a9446e77a",
+    "url": "/static/media/stolenGoods.9d6efa39.svg"
+  },
+  {
+    "revision": "867f712d711f2eaa4e3ddb0e08e1503c",
+    "url": "/static/media/bobbyPin.867f712d.svg"
+  },
+  {
+    "revision": "76a823a7ffed72040aec551d2c21ba7a",
+    "url": "/static/media/mapIcon.76a823a7.svg"
+  },
+  {
+    "revision": "534f6b509093330a47d0bd095116a0ce",
+    "url": "/static/media/chartIcon.534f6b50.svg"
+  },
+  {
+    "revision": "537fcbc654bd47c6089da403104bb2ec",
+    "url": "/index.html"
+  }
+];
